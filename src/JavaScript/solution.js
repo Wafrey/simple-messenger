f2b8bff53eeab727ba6ca328ab597bb0e478db57
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-07-05 17:49:31 +0300

    Add firebase and js

--- src/JavaScript/solution.js
+++ src/JavaScript/solution.js
@@ -1,3 +1,28 @@
 function attachEvents() {
 
+    const url = `https://messanger-b24ae.firebaseio.com/messages.json`;
+    let firstMessage = 0;
+    let firstMessageKey = ``;
+    let historyClicked = false;
+    let username = ``;
+
+    $('#author').on('click', () => {
+        $('#author').attr('placeholder', '');
+    });
+
+    $('#content').on('click', () => {
+        $('#content').attr('placeholder', '');
+    });
+
+    $('#submit').on('click', send => {
+        let author = $('#author').val();
+        let content = $('#content').val();
+        let timeStamp = Date.now();
+        let message;
+
+        if (author === '' && firstMessage === 0) {
+            $('#error').text('Please enter your name!');
+            return;
+        }
+    });
 }
