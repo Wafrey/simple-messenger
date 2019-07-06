087b486cdedef607103083e8d2fefc0ed6e108a1
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-07-06 11:10:11 +0300

    Add Ajax

--- src/JavaScript/solution.js
+++ src/JavaScript/solution.js
@@ -14,7 +14,7 @@ function attachEvents() {
         $('#content').attr('placeholder', '');
     });
 
-    $('#submit').on('click', send => {
+    $('#submit').on('click', sendMsg => {
         let author = $('#author').val();
         let content = $('#content').val();
         let timeStamp = Date.now();
@@ -23,6 +23,39 @@ function attachEvents() {
         if (author === '' && firstMessage === 0) {
             $('#error').text('Please enter your name!');
             return;
+        } else if (content.includes('<script>') || content.includes('</script>')) {
+            $('#error').text('For Security purposes <script> is forbidden!');
+            return;
+        } else {
+            firstMessage++;
         }
+
+        if (author !== '') {
+            username = author;
+            message = {
+                author: author,
+                content: content,
+                timeStamp: timeStamp
+            };
+        } else {
+            message = {
+                author: username,
+                content: content,
+                timeStamp: timeStamp
+            }
+        }
+
+        $('#author').attr('placeholder', `Your name "${username}" is now autofill`);
+        $('#content').attr('placeholder', 'Enter next message');
+
+        $.ajax({
+            method: 'POST',
+            url: url,
+            data: JSON.stringify(message),
+            success: (id) => {
+                $('#author').val('');
+                $('#content').val('');
+            }
+        })
     });
 }
