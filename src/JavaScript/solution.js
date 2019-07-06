1e35a4cb94c576f22a681040db19fd6daa05dfae
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-07-07 00:28:28 +0300

    Add chat send

--- src/JavaScript/solution.js
+++ src/JavaScript/solution.js
@@ -8,10 +8,12 @@ function attachEvents() {
 
     $('#author').on('click', () => {
         $('#author').attr('placeholder', '');
+        $('#error').hide();
     });
 
     $('#content').on('click', () => {
         $('#content').attr('placeholder', '');
+        $('#error').hide();
     });
 
     $('#submit').on('click', sendMsg => {
@@ -22,9 +24,11 @@ function attachEvents() {
 
         if (author === '' && firstMessage === 0) {
             $('#error').text('Please enter your name!');
+            $('#error').show();
             return;
         } else if (content.includes('<script>') || content.includes('</script>')) {
             $('#error').text('For Security purposes <script> is forbidden!');
+            $('#error').show();
             return;
         } else {
             firstMessage++;
@@ -55,7 +59,73 @@ function attachEvents() {
             success: (id) => {
                 $('#author').val('');
                 $('#content').val('');
+
+                if (firstMessage === 1) {
+                    firstMessageKey = id.name;
+                }
+
+                $.ajax({
+                    method: 'GET',
+                    url: url,
+                    success: (data) => {
+                        let flag = false;
+                        let profileName = '';
+                        let newMessage = '';
+                        let div = ``;
+
+                        for (let [key, message] of Object.entries(data)) {
+
+                            if (key === firstMessageKey) {
+                                flag = true;
+                            }
+                            if (flag && !historyClicked) {
+
+                                profileName = `${message.author}\n`;
+                                newMessage = `${message.content}\n`;
+
+                                div = $(`<div id="chat-messages"><div class="profile my-profile"><img src="https://static1.fjcdn.com/comments/Its+all+a+matter+of+opinion+sasuke+sama+says+u+_0b37c51c0a6781602ca2e22ff007692d.jpg" width="30" height="30"/><span>${profileName}</span></div><div class="message my-message">${newMessage}</div></div>`);
+
+                            } else if (historyClicked) {
+                                $('#refresh').click();
+                            }
+                        }
+                        
+                        $('.chat-box-body').append(div);
+                    }
+                })
             }
         })
+    });
+
+
+    $('#delete').on('click', delHistory => {
+        let clearHistory = false;
+        delHistory.preventDefault();
+        swal({
+            title: "This will delete your chat history",
+            text: "Once deleted, you will not be able to recover the messages, are you sure you want to do it?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDel) => {
+            if (willDel) {
+                swal('The history of all messages has been successfully deleted!', {
+                    icon: 'success',
+                });
+
+                $.ajax({
+                    method: 'DELETE',
+                    url: url,
+                    success: () => {
+                        $('#error').text('There is no history!');
+                        $('#error').show();
+                        $('.profile.my-profile').hide();
+                        $('.message.my-message').hide();
+                    }
+                });
+            } else {
+                swal('Your message history is safe!');
+            }
+        });
     });
 }
